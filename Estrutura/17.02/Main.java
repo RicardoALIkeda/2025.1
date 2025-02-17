4366774bffe1fc0fa996ef9c8f5cd6ea6de7ebc7
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2025-02-17 13:21:25 +0000

    adiciona métodos para calcular andar com maior número de moradores e total de moradores, além de ajustes de formatação

--- Estrutura/17.02/Main.java
+++ Estrutura/17.02/Main.java
@@ -1,36 +1,70 @@
-public class Main{
-    public static void main(String[] args){
-        int[][] predio = new int [10][8];
+public class Main {
+    public static void main(String[] args) {
+        int[][] predio = new int[10][8];
         preencheMat(predio);
         mostraMat(predio);
         int vazios = calcVazios(predio);
-        System.out.println("\nQtde de apartamentos vazios: "+vazios);
+        System.out.println("\nQtde de apartamentos vazios: " + vazios);
+        
+        int andarMaiorNumMoradores = calcAndarMaiorNumMoradores(predio);
+        System.out.println("Andar com maior número de moradores: " + andarMaiorNumMoradores);
+        
+        int totalMoradores = calcTotalMoradores(predio);
+        System.out.println("Quantidade total de moradores do prédio: " + totalMoradores);
     }
 
-public static int calcVazios(int [][]predio){
-    int count = 0;
-    for(int i=0; i<predio.length; i++){
-        for(int j=0; j<predio[i].length; j++){
-            if(predio[i][j] == 0){
-                count++;
+    public static int calcVazios(int[][] predio) {
+        int count = 0;
+        for (int i = 0; i < predio.length; i++) {
+            for (int j = 0; j < predio[i].length; j++) {
+                if (predio[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void preencheMat(int[][] predio) {
+        for (int i = 0; i < predio.length; i++) {
+            for (int j = 0; j < predio[i].length; j++) {
+                predio[i][j] = (int) (Math.random() * 7);
             }
         }
     }
-    return count;
-}
-public static void preencheMat(int [][] predio){
-    for(int i=0; i<predio.length; i++){
-        for (int j=0; j<predio[i].length; j++){
-            predio[i][j] = (int)(Math.random()*7);
+
+    public static void mostraMat(int[][] predio) {
+        for (int i = 0; i < predio.length; i++) {
+            System.out.println();
+            for (int j = 0; j < predio[i].length; j++) {
+                System.out.print(predio[i][j] + "\t");
+            }
         }
     }
-}
-public static void mostraMat(int[][] predio){
-    for(int i=0; i<predio.length;i++){
-        System.out.println();
-        for(int j=0; j<predio[i].length;j++){
-            System.out.print(predio[i][j]+ "\t");
-        }        
+
+    public static int calcAndarMaiorNumMoradores(int[][] predio) {
+        int maxMoradores = 0;
+        int andar = 0;
+        for (int i = 0; i < predio.length; i++) {
+            int totalMoradoresAndar = 0;
+            for (int j = 0; j < predio[i].length; j++) {
+                totalMoradoresAndar += predio[i][j];
+            }
+            if (totalMoradoresAndar > maxMoradores) {
+                maxMoradores = totalMoradoresAndar;
+                andar = i;
+            }
+        }
+        return andar;
     }
+
+    public static int calcTotalMoradores(int[][] predio) {
+        int total = 0;
+        for (int i = 0; i < predio.length; i++) {
+            for (int j = 0; j < predio[i].length; j++) {
+                total += predio[i][j];
+            }
+        }
+        return total;
     }
 }
